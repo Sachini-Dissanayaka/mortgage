42739d07ab40de0f08ef03b47a297f8316720d0f
Sachini-Dissanayaka <dmsachiniacc@gmail.com>
2021-05-30 17:48:16 +0530

    Updates

---
+++ precache-manifest.60a930904762e7147b12a9f83d298a15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c91be93c6013a7c90d5f",
+    "url": "/mortgage/static/js/runtime~main.c91be93c.js"
+  },
+  {
+    "revision": "e137657d0e0fb3d5fe07",
+    "url": "/mortgage/static/js/main.e137657d.chunk.js"
+  },
+  {
+    "revision": "35701d4cc521e38a640f",
+    "url": "/mortgage/static/js/1.35701d4c.chunk.js"
+  },
+  {
+    "revision": "e137657d0e0fb3d5fe07",
+    "url": "/mortgage/static/css/main.18eb2a40.chunk.css"
+  },
+  {
+    "revision": "624c47c128364ad38833b87ed7ba80e6",
+    "url": "/mortgage/index.html"
+  }
+];
